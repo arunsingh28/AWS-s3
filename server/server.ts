5aeb1cb199a9dddcf41e6dea4b3aec705e96d287
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-20 22:20:25 +0530

    fully working

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const multer = require('multer');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
 const app = express();
-
 
 const { uploadFile, downloadFile } = require('./utils/s3.ts')
 
@@ -11,25 +12,39 @@ app.use(cors())
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb', extended: true }));
 
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+})
+
+
 const upload = multer({
-    dest: 'uploads/',
-    filename: function (req, file, cb) {
-        cb(null, file.originalname + '-' + Date.now());
-    }
+    storage: multerS3({
+        s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname })
+        },
+        key: function (req, file, cb) {
+            // rename filename to be unique
+            cb(null, Date.now().toString() + '-' + file.originalname)
+        },  
+    })
 })
 
 app.post('/image', upload.single('image'), async (req, res) => {
     const file = req.file
     console.log(file)
-    const result = await uploadFile(file)
-    console.log('from AWS:', result)
     res.json({ msg: 'working' })
 })
 
+
+// fetch a file from s3
 app.get('/image/:id', async (req, res) => {
     const id = req.params.id
-    const img = await downloadFile(id)
-    res.send(`<img src="${img.Body}" height="100"/>`)
+    
 })
 
 
