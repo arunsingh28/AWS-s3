ec2dec605249797be8c1a6787bcedc4e04e7b7c0
Arun Singh <arunsingh28aug.as@gmail.com>
2021-07-20 17:27:44 +0530

    making input form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 function App() {
 
-  const [selected, setSelected] = useState();
-  const [isFilePicked, setIsFilePicked] = useState(false);
+
+  const [selected, setSelected] = useState<any | null>(null);
+  const [preview, setPreview] = useState<any | null>(null);
+  const file = useRef<any | null>(null);
+
+
+  useEffect(() => {
+    if (!selected) {
+      setPreview(undefined)
+      return
+    }
+    const objectUrl: String = URL.createObjectURL(selected)
+    setPreview(objectUrl)
+  }, [selected])
+
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -13,16 +26,30 @@ function App() {
 
   const changeHandler = (e: any) => {
     setSelected(e.target.files[0])
-    setIsFilePicked(true)
+    file.current.style.display = 'none'
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={handleSubmit}>
-          <input type="file" name="image" required onChange={changeHandler} />
+          <input type="file" name="image" required onChange={changeHandler} ref={file} />
           {
-            selected ? <button type="submit">Upload</button> : false
+            selected ? (
+              <div className="preView">
+                <img src={preview} />
+                {selected.name}<hr />
+                {selected.type}
+              </div>
+            ) : false
+          }
+          {
+            selected ? (
+              <>
+                <button type="submit">Upload</button>
+                <button type="reset">Reset</button>
+              </>
+            ) : false
           }
         </form>
       </header>
